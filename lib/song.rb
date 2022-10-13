f9c38254a2bd060f76884d9f11a0165386c17bbd
Jennifer Gifford <jgifford82@gmail.com>
2022-10-12 18:04:01 -0600

    commit tree 54407d4dc38d2bc006188c0e6d0c3136adcc1043

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    attr_accessor :name
+    attr_accessor :artist
+    attr_accessor :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << self.genre
+        @@artists << self.artist
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
